6bb14e0926b9dd0ce0a18e722a20c66d60f63ffa
jdelafon <julien.delafontaine@isb-sib.ch>
2016-07-29 15:31:52 +0200

    symlink. Test 9443

--- dist/scripts/test_functional.sh
+++ dist/scripts/test_functional.sh
@@ -4,9 +4,14 @@ printf "\n* Server is responding:\n\n"
 
 curl localhost:9000/
 
-printf "\n\n* Response to a dowloadRange query:\n\n"
+printf "\n\n* Response to a dowloadRange query at localhost:9000:\n\n"
 
 curl -I localhost:9000/downloadRange/test -H "Range: bytes=0-21793" -H "Connection: keep-alive"
 echo "#Characters: " $(curl -s localhost:9000/downloadRange/test -H "Range: bytes=0-21793" -H "Connection: keep-alive" | wc -c)
+
+printf "\n* Response to a dowloadRange query at localhost:9443:\n\n"
+
+curl -I localhost:9443/downloadRange/test -H "Range: bytes=0-21793" -H "Connection: keep-alive"
+echo "#Characters: " $(curl -s localhost:9443/downloadRange/test -H "Range: bytes=0-21793" -H "Connection: keep-alive" | wc -c)
 
 printf '\n'
---
+++ scripts/test_functional.sh
@@ -0,0 +1,1 @@
+../dist/scripts/test_functional.sh
