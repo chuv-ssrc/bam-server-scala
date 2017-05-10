dd84590709ce55224b163bc1f1b3512f2e60fe4a
jdelafon <julien.delafontaine@sib.swiss>
2017-05-10 16:30:41 +0200

    Adding already existing users_samples does not insert anything. Deleting missing users_samples does not delete anything.

--- app/controllers/management/UsersSamplesController.scala
+++ app/controllers/management/UsersSamplesController.scala
@@ -2,7 +2,7 @@ package controllers.management
 
 import javax.inject._
 
-import auth.{AdminAction, AuthenticatedRequest}
+import auth.{AdminAction, AuthenticatedAction, AuthenticatedRequest}
 import forms.UsersSamplesForm
 import models.UserSample
 import play.api.db.Database
@@ -24,22 +24,28 @@ class UsersSamplesController @Inject()(db: Database) extends Controller {
     */
   def addUsersSamples() = AdminAction(parse.json) { implicit request =>
 
+    val appId = request.user.appId
     val usersSamples = usersSamplesFromRequest
     val userIds = getUserIds(db, usersSamples.map(_.username))
     val sampleIds = getSampleIds(db, usersSamples.map(_.sample))
 
-    db.withConnection { conn =>
-      val unknowns: String = usersSamples.map(_ => "(?,?)").mkString(",")
-      val statement = conn.prepareStatement(s"""
+    val counts: Seq[Int] = (userIds zip sampleIds).map(us => findUsersSamples(db, us._1, us._2))
+    val notExistYet = (userIds zip sampleIds).zipWithIndex.filter(x => counts(x._2) == 0)
+    
+    if (notExistYet.nonEmpty) {
+      db.withConnection { conn =>
+        val unknowns: String = notExistYet.map(_ => "(?,?)").mkString(",")
+        val statement = conn.prepareStatement(s"""
         INSERT INTO `users_samples`(`user_id`,`sample_id`) VALUES $unknowns ;
       """)
-      (userIds zip sampleIds).zipWithIndex.foreach { case ((uid, sid), i) =>
-        statement.setInt(2*i+1, uid)
-        statement.setInt(2*i+2, sid)
+        notExistYet.foreach { case ((uid, sid), i) =>
+          statement.setInt(2 * i + 1, uid)
+          statement.setInt(2 * i + 2, sid)
+        }
+        statement.execute()
       }
-      statement.execute()
-      Ok(s"Inserted ${usersSamples.size} user-sample(s)")
     }
+    Ok(s"Inserted ${notExistYet.size} user-sample(s)")
   }
 
   /**
@@ -49,9 +55,14 @@ class UsersSamplesController @Inject()(db: Database) extends Controller {
     */
   def deleteUsersSamples() = AdminAction(parse.json) { implicit request =>
 
+    val appId = request.user.appId
     val usersSamples = usersSamplesFromRequest
     val userIds = getUserIds(db, usersSamples.map(_.username))
     val sampleIds = getSampleIds(db, usersSamples.map(_.sample))
+
+    val counts: Seq[Int] = (userIds zip sampleIds).map(us => findUsersSamples(db, us._1, us._2))
+    val alreadyExist = (userIds zip sampleIds).zipWithIndex.filter(x => counts(x._2) > 0)
+    // It is no problem in SQL to try and delete things that don't exist
 
     db.withConnection { conn =>
       (userIds zip sampleIds).foreach { case (uid, sid) =>
@@ -62,7 +73,7 @@ class UsersSamplesController @Inject()(db: Database) extends Controller {
         statement.setInt(2, sid)
         statement.execute()
       }
-      Ok(s"Deleted ${usersSamples.size} user-sample(s)")
+      Ok(s"Deleted ${alreadyExist.size} user-sample(s)")
     }
   }
 
@@ -111,6 +122,23 @@ object UsersSamplesController {
       }
       val sampleIds = sampleNames map (samplesMap(_))
       sampleIds
+    }
+  }
+
+  /**
+    * Return the number of users found with this *username* and *appId* in the database.
+    */
+  def findUsersSamples(db: Database, userId: Int, sampleId: Int): Int = {
+    db.withConnection { conn =>
+      val statement = conn.prepareStatement("SELECT `id` FROM `users_samples` WHERE user_id = ? AND sample_id = ? ;")
+      statement.setInt(1, userId)
+      statement.setInt(2, sampleId)
+      val result = statement.executeQuery()
+      var count = 0
+      while (result.next()) {
+        count += 1
+      }
+      count
     }
   }
 
--- test/scala/controllers/management/UsersSamplesSpec.scala
+++ test/scala/controllers/management/UsersSamplesSpec.scala
@@ -27,6 +27,18 @@ class UsersSamplesSpec extends PlaySpec with OneAppPerSuite with WithToken with 
       count must equal(count0 + 2)
     }
 
+    "ignore adding users_samples that already exist" in {
+      val body = Json.parse(s"""
+        { "users_samples": [{"sample": "$testkey", "username": "testuser"}, {"sample": "$inactivekey", "username": "testuser"}] }
+      """)
+      val db = dbContext.db
+      val count0 = countRows(db, "users_samples")
+      val response = route(app, FakeAdminRequest(PUT, "/users_samples").withJsonBody(body)).get
+      status(response) mustBe OK
+      val count = countRows(db, "users_samples")
+      count must equal(count0)
+    }
+
   }
 
   "Delete an attribution" should {
@@ -41,6 +53,18 @@ class UsersSamplesSpec extends PlaySpec with OneAppPerSuite with WithToken with 
       status(response) mustBe OK
       val count = countRows(db, "users_samples")
       count must equal(count0 - 2)
+    }
+
+    "ignore deleting non-existent users_samples" in {
+      val body = Json.parse(s"""
+        { "users_samples": [{"sample": "$testkey", "username": "testuser"}, {"sample": "$inactivekey", "username": "testuser"}] }
+      """)
+      val db = dbContext.db
+      val count0 = countRows(db, "users_samples")
+      val response = route(app, FakeAdminRequest(DELETE, "/users_samples").withJsonBody(body)).get
+      status(response) mustBe OK
+      val count = countRows(db, "users_samples")
+      count must equal(count0)
     }
 
   }
