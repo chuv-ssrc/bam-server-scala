e11748ef47cc94f1a2b6d2c8f2b0c2593debcb49
jdelafon <julien.delafontaine@sib.swiss>
2017-05-10 16:05:52 +0200

    Added isActive flag on users_samples and apps. Samples active by default.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -7,6 +7,7 @@ CREATE TABLE `apps` (
     `iss` VARCHAR(255) NOT NULL,
     `keyFile` VARCHAR(255) NOT NULL,
     `description` VARCHAR(255) DEFAULT NULL,
+    `isActive` TINYINT(1) NOT NULL DEFAULT 1
 );
 
 CREATE TABLE `users` (
@@ -16,7 +17,7 @@ CREATE TABLE `users` (
     `group` VARCHAR(255) DEFAULT NULL,
     `isActive` TINYINT(1) DEFAULT 0,
     `isAdmin` TINYINT(1) DEFAULT 0,
-    FOREIGN KEY (`app_id`) REFERENCES `apps`(`id`),
+    FOREIGN KEY (`app_id`) REFERENCES `apps`(`id`)
 );
 
 CREATE TABLE `samples` (
@@ -27,20 +28,21 @@ CREATE TABLE `samples` (
     `hash` VARCHAR(255) DEFAULT NULL,
     `description` VARCHAR(255) DEFAULT NULL,
     `isOndisk` TINYINT(1) DEFAULT NULL,
-    `isActive` TINYINT(1) NOT NULL DEFAULT 0
+    `isActive` TINYINT(1) NOT NULL DEFAULT 1
 );
 
 CREATE TABLE `users_samples` (
     `id` INTEGER(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `user_id` INTEGER(11) NOT NULL,
     `sample_id` INTEGER(11) NOT NULL,
+    `isActive` TINYINT(1) NOT NULL DEFAULT 1,
     FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
-    FOREIGN KEY (`sample_id`) REFERENCES `samples`(`id`),
+    FOREIGN KEY (`sample_id`) REFERENCES `samples`(`id`)
 );
 
 
-INSERT INTO `apps` VALUES (1, 'https://jdelafon.eu.auth0.com/', 'auth0.cer', 'Using a public certificate in .cer format');
-INSERT INTO `apps` VALUES (2, 'test', 'id_rsa.pub', 'Using only a public key in .pem format');
+INSERT INTO `apps` VALUES (1, 'https://jdelafon.eu.auth0.com/', 'auth0.cer', 'Using a public certificate in .cer format', 1);
+INSERT INTO `apps` VALUES (2, 'test', 'id_rsa.pub', 'Using only a public key in .pem format', 1);
 
 INSERT INTO `users` VALUES (1,1, 'testuser', NULL, 1, 0);
 INSERT INTO `users` VALUES (2,1, 'admin', NULL, 1, 1);
@@ -51,12 +53,12 @@ INSERT INTO `samples`(`name`,`filename`,`isActive`) VALUES ('sample2', 'test2.ba
 INSERT INTO `samples`(`name`,`filename`,`isActive`) VALUES ('notherekey', 'nothere.bam', 1);
 INSERT INTO `samples`(`name`,`filename`,`isActive`) VALUES ('inactivekey', 'inactive.bam', 0);
 
-INSERT INTO `users_samples` VALUES (4, 2, 1);
-INSERT INTO `users_samples` VALUES (5, 2, 2);
-INSERT INTO `users_samples` VALUES (6, 2, 3);
-INSERT INTO `users_samples` VALUES (7, 3, 1);
-INSERT INTO `users_samples` VALUES (8, 3, 2);
-INSERT INTO `users_samples` VALUES (9, 3, 3);
+INSERT INTO `users_samples` VALUES (4, 2, 1, 1);
+INSERT INTO `users_samples` VALUES (5, 2, 2, 1);
+INSERT INTO `users_samples` VALUES (6, 2, 3, 1);
+INSERT INTO `users_samples` VALUES (7, 3, 1, 1);
+INSERT INTO `users_samples` VALUES (8, 3, 2, 1);
+INSERT INTO `users_samples` VALUES (9, 3, 3, 1);
 
 
 # --- !Downs
